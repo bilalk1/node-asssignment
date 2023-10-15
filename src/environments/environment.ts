c4f117ae31202c6873740ddd5024adcbfc80dfb9
Bilal Iftikhar <bilaalahmaad1@gmail.com>
2023-10-15 19:15:50 +0500

    Update Read.me and package.json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -46,25 +46,9 @@ class Environment implements IEnvironment {
    * @param env
    */
   public setEnvironment(env: string): void {
-    let envPath: string;
     this.env = env || Environments.LOCAL;
     const rootdir: string = path.resolve(__dirname, '../../');
-    switch (env) {
-      case Environments.PRODUCTION:
-        envPath = path.resolve(rootdir, EnvironmentFile.PRODUCTION);
-        break;
-      case Environments.TEST:
-        envPath = path.resolve(rootdir, EnvironmentFile.TEST);
-        break;
-      case Environments.STAGING:
-        envPath = path.resolve(rootdir, EnvironmentFile.STAGING);
-        break;
-      case Environments.LOCAL:
-        envPath = path.resolve(rootdir, EnvironmentFile.LOCAL);
-        break;
-      default:
-        envPath = path.resolve(rootdir, EnvironmentFile.LOCAL);
-    }
+    const envPath = path.resolve(rootdir, EnvironmentFile.LOCAL);
     if (!fs.existsSync(envPath)) {
       throw new Error('.env file is missing in root directory');
     }
